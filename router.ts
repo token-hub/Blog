67d458c647a380467af7a62b0db26249a6f1e9f1
John Suyang <johnsuyang2118@gmail.com>
2026-01-13 20:05:48 +0800

    add login page

--- router.ts
+++ router.ts
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./src/components/Home";
+import Login from "./src/components/Login";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: Home,
+    },
+    {
+        path: "/login",
+        Component: Login,
     },
 ]);
---
+++ src/components/Login.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { SearchCode } from "lucide-react";
+import { Link } from "react-router";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Spinner } from "@/components/ui/spinner";
+
+const Login = () => {
+    return (
+        <div className="h-dvh">
+            <div className="h-full flex justify-center items-center h-minus-header">
+                <form onSubmit={() => {}} className="w-full max-w-sm">
+                    <Card>
+                        <CardHeader className="space-y-2 place-items-center">
+                            <Link to="/" className="hover:cursor-pointer">
+                                <SearchCode className="w-18 h-18 m-0 text-yellow-500" />
+                            </Link>
+                            <CardTitle className="text-center text-2xl">Login</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="flex flex-col gap-6">
+                                <div className="grid gap-2">
+                                    <Label htmlFor="email">
+                                        Email <span className="text-red-700">*</span>
+                                    </Label>
+                                    <Input type="email" placeholder="email@gmail.com" />
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="flex items-center">
+                                        <Label htmlFor="password">
+                                            Password <span className="text-red-700">*</span>
+                                        </Label>
+                                    </div>
+                                    <Input type="password" placeholder="********" />
+                                </div>
+                            </div>
+                        </CardContent>
+                        <CardFooter className="flex-col gap-2">
+                            <Button disabled={false} type="submit" className="w-full hover:cursor-pointer">
+                                {false && <Spinner />} Login
+                            </Button>
+
+                            <Link to="/register" className="text-sm text-center text-muted-foreground">
+                                {"don't have an account?"} <span className="text-yellow-500 font-bold">Register</span>
+                            </Link>
+                        </CardFooter>
+                    </Card>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
