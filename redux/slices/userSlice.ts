fc98bdb7b1362ed70b3059c5a122c65924989ea4
John Suyang <johnsuyang2118@gmail.com>
2026-01-13 22:42:58 +0800

    create user store

---
+++ redux/slices/userSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { User, Session } from "@supabase/supabase-js";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+type authType = {
+    user: User | null;
+    session: Session | null;
+};
+
+type userSliceState = {
+    auth: authType;
+};
+
+const initialState: userSliceState = {
+    auth: {
+        user: null,
+        session: null,
+    },
+};
+
+export const userSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        setUser: (state, action: PayloadAction<authType>) => {
+            state.auth = action.payload;
+        },
+    },
+});
+
+export const { setUser } = userSlice.actions;
+
+export default userSlice.reducer;
