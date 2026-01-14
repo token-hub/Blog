afed88d023ecc258389212b7162311a68d4ef9e9
John Suyang <johnsuyang2118@gmail.com>
2026-01-14 12:48:39 +0800

    add delete and edit buttons to blog

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -1,12 +1,25 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { capitalizedFirstLetter, transformDate } from "@/lib/utils";
 import type { blogType } from "@/lib/types";
+import { Button } from "@/components/ui/button";
+import { Trash2 } from "lucide-react";
+import { Pencil } from "lucide-react";
 
 const Blog = ({ title, created_at, blog }: blogType) => {
     return (
         <Card className="w-3/4 hover:shadow-xl hover:cursor-pointer mb-4">
             <CardContent>
-                <h1 className="text-4xl mb-4">{capitalizedFirstLetter(title)}</h1>
+                <div className="flex justify-between">
+                    <h1 className="text-4xl mb-4">{capitalizedFirstLetter(title)}</h1>
+                    <div className="flex gap-2">
+                        <Button size="sm" type="button">
+                            <Trash2 />
+                        </Button>
+                        <Button size="sm" type="button">
+                            <Pencil />
+                        </Button>
+                    </div>
+                </div>
                 <p className="text-gray mb-4">{transformDate(created_at)}</p>
                 <p className="leading-6">{blog}</p>
             </CardContent>
