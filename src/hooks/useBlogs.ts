33aa046db3742fd6a382cf46bbf8e0b9f21c73ef
John Suyang <johnsuyang2118@gmail.com>
2026-01-16 00:10:14 +0800

    refactor useBlog

--- src/hooks/useBlogs.ts
+++ src/hooks/useBlogs.ts
@@ -2,22 +2,22 @@ import { getBlogs, getBlogsCount } from "@/redux/action-creators/blogActions";
 import type { RootState, AppDispatch } from "@/redux/store";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useSearchParams } from "react-router";
 
 import type { User } from "@supabase/supabase-js";
+import { usePage } from "./usePage";
+import { PAGINATION_IDENTIFIER } from "@/lib/constants";
 
 export const useBlogs = ({ user }: { user: User | null }) => {
     const dispatch = useDispatch<AppDispatch>();
     const { blogs, totalCount, loading } = useSelector((state: RootState) => state.blog);
-    const [searchParams] = useSearchParams();
-    const page = searchParams.get("page") ?? 1;
+    const page = usePage(PAGINATION_IDENTIFIER[0]);
 
     useEffect(() => {
         dispatch(getBlogsCount(user?.id));
     }, [dispatch, user?.id]);
 
     useEffect(() => {
-        dispatch(getBlogs({ page: +page, user_id: user?.id }));
+        dispatch(getBlogs({ page, user_id: user?.id }));
     }, [dispatch, page, user?.id]);
 
     return { blogs, totalCount, loading };
