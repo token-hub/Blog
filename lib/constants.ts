e0ec34785a4c715d05250bec92cb162d080de8f0
John Suyang <johnsuyang2118@gmail.com>
2026-01-15 21:25:07 +0800

    Add action for fetching comments

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,6 +21,7 @@ export const TABLES = ["blogs", "comments"] as const;
 export const ERROR_CODES = ["23505"];
 export const ERROR_CODES_DESCRIPTION = ["A blog with this title already exists"];
 export const BLOG_LIMIT = 3;
+export const COMMENT_LIMIT = 3;
 export const SET_BLOGS_COUNT = ["increment", "decrement"] as const;
 export const TOAST_TYPE = ["success", "error"] as const;
 
--- redux/action-creators/commentActions.ts
+++ redux/action-creators/commentActions.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { supabase } from "../../lib/supabase";
 import { uploadImageToClaudinary } from "../../lib/utils";
 import type { insertCommentType } from "../../lib/types";
-import { TABLES } from "../../lib/constants";
+import { COMMENT_LIMIT, TABLES } from "../../lib/constants";
 
 export const createComment = createAsyncThunk("comments/createComment", async (fields: insertCommentType, thunkApi) => {
     try {
@@ -16,6 +16,27 @@ export const createComment = createAsyncThunk("comments/createComment", async (f
             image_url,
         });
 
+        if (error) throw error;
+        return data;
+    } catch (error: any) {
+        const message = error.message;
+        return thunkApi.rejectWithValue(message);
+    }
+});
+
+export const getComments = createAsyncThunk("comments/getComments", async ({ page, blog_id }: { page: number; blog_id: string }, thunkApi) => {
+    try {
+        const offset = (page - 1) * COMMENT_LIMIT;
+
+        const query = supabase
+            .from("comments")
+            .select(`created_at, id, comment, image_url, user: user_profiles (id, email)`)
+            .eq("blog_id", blog_id)
+            .order("created_at", { ascending: false })
+            .limit(COMMENT_LIMIT)
+            .range(offset, offset + COMMENT_LIMIT - 1);
+
+        const { data, error } = await query;
         if (error) throw error;
         return data;
     } catch (error: any) {
